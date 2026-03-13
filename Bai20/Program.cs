b6863c7c2fd43a996ee19ff7592ccb9e317a905a
nhatphuong-02 <phuongquachnhat@gmail.com>
2026-03-13 18:30:49 +0700

    Bai20

---
+++ Bai20/Program.cs
@@ -0,0 +1,9 @@
+﻿using SinhVien;
+Console.WriteLine("Quach Nhat Phuong\nMa sinh vien: 2415053122336\n");
+List<Student> students = new List<Student>() {  new Student { Id = 1, Name = "An", Score = 8 }, 
+                                                new Student { Id = 2, Name = "Binh", Score = 6 }, 
+                                                new Student { Id = 3, Name = "Chi", Score = 9 },
+                                                new Student { Id = 4, Name = "Dung", Score = 7 } };
+var bai20 = students.OrderByDescending(x => x.Score).Take(3).ToList();
+Console.WriteLine("Top 3 sinh vien diem cao nhat la: ");
+bai20.ForEach(x => Console.WriteLine($"Ten: {x.Name}; Diem: {x.Score}"));
