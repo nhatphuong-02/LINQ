79246362278164aeef342779427838e6ef05262d
nhatphuong-02 <phuongquachnhat@gmail.com>
2026-03-13 18:21:22 +0700

    Bai12

---
+++ Bai12/Program.cs
@@ -0,0 +1,10 @@
+﻿using SinhVien;
+
+Console.WriteLine("Quach Nhat Phuong\nMa sinh vien: 2415053122336\n");
+List<Student> students = new List<Student>() {  new Student { Id = 1, Name = "An", Score = 8 },
+                                                new Student { Id = 2, Name = "Binh", Score = 6 },
+                                                new Student { Id = 3, Name = "Chi", Score = 9 },
+                                                new Student { Id = 4, Name = "Dung", Score = 7 } };
+
+var bai12 = students.Where(student => student.Score >=7).Count();
+Console.Write("So sinh vien dat: " + bai12);
