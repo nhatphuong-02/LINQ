6738337cadb1249927f3722d307a05bc0df89516
nhatphuong-02 <phuongquachnhat@gmail.com>
2026-03-13 18:35:10 +0700

    LopSinhVienDungChung

---
+++ SinhVien/SinhVien.cs
@@ -0,0 +1,9 @@
+﻿namespace SinhVien
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Score { get; set; }
+    }
+}
