7822143765b25ba8bb0b3f620b24d553c614bfed
nhatphuong-02 <phuongquachnhat@gmail.com>
2026-03-13 18:19:44 +0700

    Bai9

---
+++ Bai9/Program.cs
@@ -0,0 +1,11 @@
+﻿using SinhVien;
+
+Console.WriteLine("Quach Nhat Phuong\nMa sinh vien: 2415053122336\n");
+List<Student> students = new List<Student>() {  new Student { Id = 1, Name = "An", Score = 8 },
+                                                new Student { Id = 2, Name = "Binh", Score = 6 },
+                                                new Student { Id = 3, Name = "Chi", Score = 9 },
+                                                new Student { Id = 4, Name = "Dung", Score = 7 } };
+var bai9 = students.Select(student => student.Name).ToList();
+
+Console.Write("Cac sinh vien gom: ");
+bai9.ForEach(x => Console.Write(x + ", "));
