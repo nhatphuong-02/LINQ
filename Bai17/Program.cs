bdfa14bcb6d77bff09a105a1309cde8f03d1ae53
nhatphuong-02 <phuongquachnhat@gmail.com>
2026-03-13 18:27:23 +0700

    Bai17

---
+++ Bai17/Program.cs
@@ -0,0 +1,9 @@
+﻿using SinhVien;
+
+Console.WriteLine("Quach Nhat Phuong\nMa sinh vien: 2415053122336\n");
+List<Student> students = new List<Student>() {  new Student { Id = 1, Name = "An", Score = 8 }, 
+                                                new Student { Id = 2, Name = "Binh", Score = 6 }, 
+                                                new Student { Id = 3, Name = "Chi", Score = 9 }, 
+                                                new Student { Id = 4, Name = "Dung", Score = 7 }};
+var bai17 = students.Average(x => x.Score);
+Console.WriteLine("Diem trung binh sinh vien la: " + bai17);
