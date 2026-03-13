f42469bf91e2dd98c1f8cce3bb56b84bee7724cb
nhatphuong-02 <phuongquachnhat@gmail.com>
2026-03-13 18:26:45 +0700

    Bai16

---
+++ Bai16/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Quach Nhat Phuong\nMa sinh vien: 2415053122336\n");
+List<int> num  = new List<int> { 1,2,3,4,5,6,7,8,9};
+var bai16 = num.Sum();
+Console.WriteLine("Tong cac phan tu trong mang la: " + bai16);
