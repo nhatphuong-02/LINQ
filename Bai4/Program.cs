b0db16f6942c9958a00a0e807be08e36b014bb1f
nhatphuong-02 <phuongquachnhat@gmail.com>
2026-03-13 18:12:03 +0700

    Bai4

---
+++ Bai4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Quach Nhat Phuong\nMa sinh vien: 2415053122336\n");
+
+List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+var bai4 = numbers.OrderByDescending(x => x).ToList();
+
+Console.Write("Danh sach giam dan: ");
+bai4.ForEach(x => Console.Write(x + " "));
+
+
