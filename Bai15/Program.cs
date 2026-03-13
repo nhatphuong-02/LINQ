b7ce2b4b6229015ff48b8d7f7d321238ce90f808
nhatphuong-02 <phuongquachnhat@gmail.com>
2026-03-13 18:26:17 +0700

    Bai15

---
+++ Bai15/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Quach Nhat Phuong\nMa sinh vien: 2415053122336\n");
+
+List<int> num = new List<int> { 1,2,2,3,4,4,5 };
+var bai15 = num.Distinct().ToList();
+
+Console.Write("Danh sach gom: ");
+bai15.ForEach(x=> Console.Write(x+" "));
