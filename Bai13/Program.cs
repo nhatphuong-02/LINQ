5ece64b4a54dde96adf695944f4f71b142f10e08
nhatphuong-02 <phuongquachnhat@gmail.com>
2026-03-13 18:22:32 +0700

    Bai13

---
+++ Bai13/Program.cs
@@ -0,0 +1,10 @@
+﻿using SinhVien;
+
+Console.WriteLine("Quach Nhat Phuong\nMa sinh vien: 2415053122336\n");
+List<Student> students = new List<Student>() {  new Student { Id = 1, Name = "An", Score = 8 },
+                                                new Student { Id = 2, Name = "Binh", Score = 6 },
+                                                new Student { Id = 3, Name = "Chi", Score = 9 },
+                                                new Student { Id = 4, Name = "Dung", Score = 7 } };
+
+var bai13 = students.FirstOrDefault(x => x.Score > 7);
+Console.WriteLine($"Sinh vien dau tien co diem >7: {bai13.Name}, {bai13.Score}");
