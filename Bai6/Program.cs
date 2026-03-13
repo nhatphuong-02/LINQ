9a41c8834a8d5607aac956d19ea353c69f5a365f
nhatphuong-02 <phuongquachnhat@gmail.com>
2026-03-13 18:13:57 +0700

    Bai6

---
+++ Bai6/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Quach Nhat Phuong\nMa sinh vien: 2415053122336\n");
+List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+var bai6 = numbers.Count(x => x%2==0);
+
+Console.WriteLine("Co {0} so chan trong danh sach",bai6);
