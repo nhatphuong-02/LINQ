faf8104c07a5e01330a63c30079276e72c53c35a
nhatphuong-02 <phuongquachnhat@gmail.com>
2026-03-13 18:10:40 +0700

    Bai2

---
+++ Bai2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Quach Nhat Phuong\nMa sinh vien: 2415053122336\n");
+
+List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+var bai2 = numbers.Where(x => x>5).ToList();
+
+Console.Write("Cac so lon hon 5: ");
+bai2.ForEach(x => Console.Write(x + " "));
+
