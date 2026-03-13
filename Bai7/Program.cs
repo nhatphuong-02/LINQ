0653881d00e3d7fab615f1a560fed7c69572a9ad
nhatphuong-02 <phuongquachnhat@gmail.com>
2026-03-13 18:18:38 +0700

    Bai7

---
+++ Bai7/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Quach Nhat Phuong\nMa sinh vien: 2415053122336\n");
+List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8};
+var bai7 = numbers.Any(x => x > 10);
+
+Console.WriteLine(bai7 ? "Co so lon hon 10" : "Khong co so lon hon 10");
