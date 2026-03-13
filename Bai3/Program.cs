27152b266ec1e9eacf7ae5c94d41903d12ec13a7
nhatphuong-02 <phuongquachnhat@gmail.com>
2026-03-13 18:11:27 +0700

    Bai3

---
+++ Bai3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Quach Nhat Phuong\nMa sinh vien: 2415053122336\n");
+
+List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
+var bai3 = numbers.OrderBy(x => x).ToList();
+
+Console.Write("Danh sach tang dan: ");
+bai3.ForEach(x => Console.Write(x + " "));
+
