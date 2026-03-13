95c9e6a130160ae8e1befc163fbc776ac436b973
nhatphuong-02 <phuongquachnhat@gmail.com>
2026-03-13 18:30:14 +0700

    Bai19

---
+++ Bai19/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Quach Nhat Phuong\nMa sinh vien: 2415053122336\n");
+List<int> numbers = new List<int> { 1, 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5};
+var bai19 = numbers.GroupBy(x => x).OrderByDescending(x => x.Count()).First();
+Console.WriteLine($"Phan tu xuat hien nhieu nhat la: {bai19.Key}\nSo lan xuat hien: {bai19.Count()}");
