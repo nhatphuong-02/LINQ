77509afb8f51f63bb93d00e34d3e8b13a6942f8c
nhatphuong-02 <phuongquachnhat@gmail.com>
2026-03-14 14:07:49 +0700

    BaiTapTrenLop

---
+++ BaiTapTrenLop/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tuan6;
+using System.Linq;
+
+Console.WriteLine("Quach Nhat Phuong\nMa sinh vien: 2415053122336\n");
+
+List<SinhVien> list = new List<SinhVien>();
+Random random = new Random();
+for(int i=0; i<20; i++)
+{
+    SinhVien sv = new SinhVien();
+    sv.Ten = "SinhVien" + i;
+    sv.Tuoi = random.Next(18, 25);
+    sv.Khoa = "CNS";
+    sv.Dtb = random.NextDouble() * 10;
+    sv.NamHoc = random.Next(1, 5);
+    list.Add(sv);
+}
+
+//Cau 1
+var maxTuoi = list.Max(x => x.Tuoi);
+Console.WriteLine("Tuoi lon nhat la: " + maxTuoi);
+var minTuoi = list.Min(x => x.Tuoi);
+Console.WriteLine("Tuoi nho nhat la: " + minTuoi);
+
+//Cau 2
+if(list.Any(x => x.Khoa == "CNS"))
+    Console.WriteLine("Co sinh vien khoa CNS");
+else
+    Console.WriteLine("Khong co sinh vien khoa CNS");
+
+//Cau 3
+var cau3 = list.OrderByDescending(x => x.Dtb).Take(10).ToList();
+Console.WriteLine("\nDanh sach 10 sinh vien diem cao nhat khoa la:");
+cau3.ForEach(x=> Console.WriteLine($"Ten: {x.Ten}; Nam hoc: {x.NamHoc}; Diem: {x.Dtb:F2}"));
+
+//Cau 4
+var cau4 = list.Where(x => x.NamHoc != 4).ToList();
+Console.WriteLine("\nDanh sach sinh vien nam 1->3:");
+cau4.ForEach(x => Console.WriteLine($"Ten: {x.Ten}; Nam hoc: {x.NamHoc}"));
---
+++ BaiTapTrenLop/SinhVien.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tuan6
+{
+    public class SinhVien
+    {
+        private String ten;
+        private int tuoi;
+        private String khoa;
+        private double dtb;
+        private int namHoc;
+
+        public String Ten { get; set; }
+        public int Tuoi { get; set; }
+        public String Khoa { get; set; }
+        public double Dtb { get; set; }
+        public int NamHoc {  get; set; }
+
+        public SinhVien() { }
+        public SinhVien(string ten, int tuoi, string khoa, double dtb, int namHoc)
+        {
+            Ten = ten;
+            Tuoi = tuoi;
+            Khoa = khoa;
+            Dtb = dtb;
+            NamHoc = namHoc;
+        }
+
+        public void Display() 
+        {
+            Console.WriteLine("Ten: " + Ten);
+            Console.WriteLine("Tuoi: " + Tuoi);
+            Console.WriteLine("Khoa: " + Khoa);
+            Console.WriteLine("Diem trung binh: "+Dtb);
+            Console.WriteLine("Nam hoc: " + NamHoc);
+        }
+    }
+}
